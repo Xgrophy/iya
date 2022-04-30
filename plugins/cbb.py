ef3b140aebd4d973d5b4f0b97341519fe2364928
OpyRights <102116409+Xgrophy@users.noreply.github.com>
2022-04-30 07:38:04 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -5,7 +5,7 @@
 from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
 
 from bot import Bot
-from config import CHANNEL, GROUP, OWNER
+from config import OWNER
 
 
 @Bot.on_callback_query()
@@ -13,7 +13,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text=f"<b>Tentang Bot ini:\n\n • Owner: @{OWNER}\n • Channel: @{CHANNEL}\n • Group: @{GROUP}\n • Source Code: <a href='https://github.com/mrismanaziz/File-Sharing-Man'>Klik Disini</a>\n • Owner Repo: @mrismanaziz</b>\n",
+            text=f"<b>Owner: @{OWNER}\n • Order Vcs: @Riciiila\n • Support by @Lunatic0de</b>\n",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("• ᴛᴜᴛᴜᴘ •", callback_data="close")]]
